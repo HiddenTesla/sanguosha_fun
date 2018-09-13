de83fdb66ed93a3751d8d305fd7563ff819165fa
daot <chen90071@163.com>
2018-09-13 23:30:17 +0800

    Improve skill gozong

--- fun2.lua
+++ fun2.lua
@@ -3,7 +3,7 @@ extension = sgs.Package("fun2")
 
 guaitai=sgs.General(extension, "guaitai$","shu", 5, false,true)
 BTliubei=sgs.General(extension, "BTliubei$","shu", 5, true)
-BTsunquan=sgs.General(extension, "BTsunquan$","wu", 5, true)
+BTsunquan=sgs.General(extension, "BTsunquan$","wu", 3, true)
 lingxi=sgs.General(extension, "lingxi","wu", 3, false, true)
 
 mashu2 = sgs.CreateDistanceSkill{
@@ -609,7 +609,7 @@ guzong_extra = sgs.CreateMaxCardsSkill {
 
 guzong = sgs.CreateTriggerSkill {
     name = "guzong",
-    events = {sgs.CardsMoveOneTime},
+    events = {sgs.CardsMoveOneTime, sgs.DrawNCards},
     frequency = sgs.Skill_Compulsory,
     on_trigger = function(self, event, player, data)
         local room = player:getRoom()
@@ -622,6 +622,12 @@ guzong = sgs.CreateTriggerSkill {
                 (bit32.band(move.reason.m_reason, sgs.CardMoveReason_S_MASK_BASIC_REASON) == sgs.CardMoveReason_S_REASON_DISCARD)
             then
                 player:gainMark("@guzong", 1)
+            end
+        elseif event == sgs.DrawNCards then
+            local extra = player:getMark("@guzong")
+            if extra > 0 then
+                local count = data:toInt() + extra
+                data:setValue(count)
             end
         end
     end
@@ -683,5 +689,5 @@ sgs.LoadTranslationTable{
     ["dutao"] = "毒桃",
     [":dutao"] = "<b>反贼技，锁定技，</b>每当内奸对一名角色使用【桃】时，该【桃】无效，该角色失去1点体力上限且该内奸立即死亡。",
     ["guzong"] = "故纵",    
-    [":guzong"] = "<b>锁定技，</b>若你于弃牌阶段弃掉的牌不少于2张，你的手牌上限永久+1。", 
+    [":guzong"] = "<b>锁定技，</b>若你于弃牌阶段弃掉的牌不少于2张，你获得1个故纵标记。摸牌阶段，你额外摸X张牌，你的手牌上限+X （X为故纵标记的数量）。", 
 }
